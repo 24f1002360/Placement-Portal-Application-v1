e65d86798cdfeb64abb1ed16e8e7ec10e5fce388
Gaurav  Dalal <gauravdalal@Gauravs-MacBook-Air-2.local>
2026-03-10 20:07:34 +0530

    Milestone-PPA Auth_RBAC

--- app.py
+++ app.py
@@ -20,22 +20,77 @@ def index():
 def login():
     if request.method=='GET' :
         return render_template("login.html")
-    if request.method=='POST' :
-        name=request.form.get('username')
-        return render_template("admin.html", username=name)
-        
+    username = request.form.get("username")
+    password=request.form.get("password")
+    
+    admin = Admin.query.filter_by(username=username , password=password).first()
+    if admin:
+        return redirect("/admin")
+    
+    student= Student.query.filter_by(name = username , password=password).first()
+    if student:
+        return redirect("/student")
+    
+    company = Company.query.filter_by(company_name=username , password=password).first()
+    if company:    
+        if company.approval_status !="approved":
+            return "Company not approved by admin yet "
+        return redirect("/company")
+    
+    return "Invalid credentials"
+
+    
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method=='GET' :
         return render_template("register.html")
     if request.method=='POST' :
-        return render_template("home.html")
+        
+        role = request.form.get('role')
+        name= request.form.get('username')
+        email= request.form.get('email')
+        password= request.form.get('password')
+        
+        student_exist = Student.query.filter_by(email=email).first()
+        company_exist = Company.query.filter_by(email=email).first()
+        if student_exist or company_exist:
+            return "Email already registered"
+        
+        if role == 'student':
+            student = Student(
+                name=name,
+                email=email,
+                password=password
+            )
+            db.session.add(student)
+        elif role == 'company':
+            company = Company(
+                company_name=name,
+                email=email,
+                password=password
+            )
+            db.session.add(company)
+            
+            
+
+        db.session.commit()
+        return redirect("/login")
+        
         
 
 @app.route('/admin', methods=['GET', 'POST'])
-def admin():
+def admin_dashboard():
         return render_template("admin.html")
     
+@app.route("/student")
+def student_dashboard():
+    return render_template("student.html")
+
+
+@app.route("/company")
+def company_dashboard():
+    return render_template("company.html")
+
 
 with app.app_context():
 
